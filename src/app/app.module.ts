44d247484c5bdb0ca2959aed5db8207bc5f9236a
Fenna <Fennahornstra@gmail.com>
2021-09-16 15:22:45 +0200

    hello

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PhotoGridComponent } from './photo-grid/photo-grid.component';
+import { PhotoDetailComponent } from './photo-detail/photo-detail.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhotoGridComponent,
+    PhotoDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/photo-grid/photo-grid.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { GetPhotosService } from '../core/get-photos.service';
+import { Photo } from '../core/Photo.model';
+
+@Component({
+  selector: 'app-photo-grid',
+  templateUrl: './photo-grid.component.html',
+  styleUrls: ['./photo-grid.component.scss']
+})
+export class PhotoGridComponent implements OnInit {
+  photos: Photo[] = [];
+  show: Boolean = false;
+  constructor(private PhotoService: GetPhotosService, private router: Router) {
+  }
+  ngOnInit(): void {
+
+    this.PhotoService.GetPhotos().subscribe(x => this.photos = x.slice(0, 50));
+    this.show = false
+
+  }
+
+  onClick(id: number) {
+    this.PhotoService.GetId(id, true)
+    this.router.navigate(['/Photo'])
+  }
+  showImg(id: number) {
+    console.log("at click")
+    this.PhotoService.GetId(id, false);
+    this.show = true;
+  }
+
+}
